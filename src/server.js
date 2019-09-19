3343d0aedf5db9f9ffa2d432aca35de3c07da7ec
Letícia <leticia.faria@dasa.com.br>
2019-09-19 16:14:57 -0300

    Create endpoints

--- src/server.js
+++ src/server.js
@@ -1,8 +1,43 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send('MVP - Controle de senha nas unidades');
+app.use(bodyParser.json())
+
+//  Add patient to queue
+app.post('/addQueue', (req, res) => {
+  let patient = false;
+
+  (patient == true)
+    ?
+    res.json({
+      "exist": true,
+      "patient": {
+        "cpf": req.body.cpf,
+        "name": null,
+        "priority": null
+      }
+    })
+    :
+    res.json({
+      "exist": false
+    });
+});
+
+// Create patient and add to queue
+app.post('/upQueue', (req, res) => {
+  res.json({
+    "patient": {
+      "cpf": req.body.cpf,
+      "name": req.body.name,
+      "priority": req.body.priority
+    }
+  });
+});
+
+// Remove patient from queue
+app.delete('/removeFromQueue', (req, res) => {
+  res.send(`${req.body.cpf} retirado da lista`);
 });
 
 app.listen(4000, () => {
